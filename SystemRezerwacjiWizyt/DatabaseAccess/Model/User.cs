bf6f0cc52e82f86a0bc57a88a90ce9f1fdbf73c0
Pienio <pienbartl@gmail.com>
2017-01-16 18:10:03 +0100

    Poprawka Rejestracji

--- SystemRezerwacjiWizyt/DatabaseAccess/Model/User.cs
+++ SystemRezerwacjiWizyt/DatabaseAccess/Model/User.cs
@@ -11,32 +11,32 @@ namespace DatabaseAccess.Model
     [DataContract(IsReference = true)]
     public class User : Entity
     {
-        [Required]
-        [DataMember]
-        [Display(Name = "PESEL")]
-        [StringLength(11, ErrorMessage = "{0} musi mieć dokładnie 11 cyfr.", MinimumLength = 11)]
-        [RegularExpression("^[0-9]*$", ErrorMessage = "Pesel może zawierać tylko cyfry")]
+        //[Required]
+        //[DataMember]
+        //[Display(Name = "PESEL")]
+        //[StringLength(11, ErrorMessage = "{0} musi mieć dokładnie 11 cyfr.", MinimumLength = 11)]
+        //[RegularExpression("^[0-9]*$", ErrorMessage = "Pesel może zawierać tylko cyfry")]
         public string PESEL { get; set; }
-        [Required]
-        [DataMember]
-        [StringLength(100, ErrorMessage = "{0} musi mieć przynajmniej {2} znaków.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Hasło")]
+        //[Required]
+        //[DataMember]
+        //[StringLength(100, ErrorMessage = "{0} musi mieć przynajmniej {2} znaków.", MinimumLength = 6)]
+        //[DataType(DataType.Password)]
+        //[Display(Name = "Hasło")]
         public string Password { get; set; }
-        [Required]
-        [DataMember]
+        //[Required]
+        //[DataMember]
         public virtual PersonName Name { get; set; } = new PersonName();
-        [Required]
-        [DataMember]
+        //[Required]
+        //[DataMember]
         public DocOrPat Kind { get; set; }
-        [Required]
-        [DataMember]
+        //[Required]
+        //[DataMember]
         public bool Active { get; set; } = true;
-        [Required]
-        [DataMember]
-        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
-                        @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
-                        @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Wprowadź poprawny email")]
+        //[Required]
+        //[DataMember]
+        //[RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+        //                @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+        //                @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Wprowadź poprawny email")]
         public string Mail { get; set; }
     }
 }
